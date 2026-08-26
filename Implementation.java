c0c0d51aa2679ecb6b37e3d677cf5dacead26796
codewith127 <codewith127@gmail.com>
2026-08-26 15:38:22 +0530

    added the insertAtEnd function for LinkedList

---
+++ Implementation.java
@@ -0,0 +1,41 @@
+
+public class Implementation {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+        public static class linkedlist {
+            Node head = null;
+            Node tail = null;
+
+            void insertAtEnd(int val) {
+                Node temp = new Node(val);
+                if (head == null) {
+                    head = temp;
+                } else {
+                    tail.next = temp;
+                }
+                tail = temp;
+            }
+
+            void display() {
+                Node temp = head;
+                while (temp != null) {
+                    System.out.print(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+
+    public static void main(String[] args) {
+        linkedlist ll = new linkedlist();
+        ll.insertAtEnd(7);
+        ll.insertAtEnd(8);
+        ll.display();
+    }
+}

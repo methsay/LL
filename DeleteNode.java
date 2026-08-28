e222b593f9553d11db6cc5ea379ffbf10870d51e
codewith127 <codewith127@gmail.com>
2026-08-29 02:24:44 +0530

    Leetcode237 delete a node

---
+++ DeleteNode.java
@@ -0,0 +1,17 @@
+class ListNode{
+    int data;
+    ListNode next;
+    ListNode(int data){
+        this.data = data;
+    }
+    static void deleteNode(ListNode node){
+        node.data = node.next.data;
+        node.next = node.next.next;
+    }
+}
+
+public class DeleteNode{
+    public static void main(String[] args) {
+        //pass the node which you want to delete //leetcode237
+    }
+}

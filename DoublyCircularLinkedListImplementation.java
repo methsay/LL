b5ae10dda3e0f6cc4fde19c4597ed9495680683c
Yash <jhaom574@gmail.com>
2026-09-03 11:55:18 +0530

    created the Doubly linked list

--- DoublyCircularLinkedListImplementation.java
+++ DoublyCircularLinkedListImplementation.java
@@ -1,10 +1,28 @@
 public class DoublyCircularLinkedListImplementation {
-    class Node{ //Doubly Linked List Class
+    static class Node{ //Doubly Linked List Class
         int val;
         Node next;
         Node prev;
         Node(int val){ //constructor
             this.val = val;
         }
+    }
+    public static void main(String[] args) {
+        // 4 10 2 99 13
+        Node a = new Node(4);
+        Node b = new Node(10);
+        Node c = new Node(2);
+        Node d = new Node(99);
+        Node e = new Node(13);
+        a.prev = null;
+        a.next = b;
+        b.prev = a;
+        b.next = c;
+        c.prev = b;
+        c.next = d;
+        d.prev = c;
+        d.next = e;
+        e.prev = d;
+        e.next = null;
     }
 }
